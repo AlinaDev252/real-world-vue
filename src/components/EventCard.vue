878751fbf611416a483eaf88764ffad8786109a6
AlinaDev252 <costachealina25@gmail.com>
2021-05-11 14:02:33 +0300

    Add settings for VScode optimization

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 v-if=""></h1>
+</template>
+
+<script>
+export default {
+props: {
+  propName: {
+    type: Number,
+    default:  
+  },
+},
+data() {
+  return {
+    key: value
+  }
+},
+}
+</script>
+
+<style lang="scss" scoped></style>
