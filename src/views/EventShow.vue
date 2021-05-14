cb4d62d81e775c0383dc7abafa88a49230eca31d
AlinaDev252 <costachealina25@gmail.com>
2021-05-14 15:49:36 +0300

    Implement the Show Event Page

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Showing Event #{{ id }}</h1>
+  <h1>Showing Event {{ event.title }}</h1>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
 export default {
   props: ['id'],
+  data() {
+    return {
+      event: {},
+    }
+  },
+  created() {
+    EventService.getEvent(this.id) // <--- Send the prop id to our EventService
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.log('There was an error:', error.response)
+      })
+  },
 }
 </script>
